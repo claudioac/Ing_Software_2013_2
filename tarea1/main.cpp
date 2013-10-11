2dca0681cd9d2e6fd41e38fecd4873975c140b8d
pepitojose <joseacunaacuna@gmail.com>
2013-10-10 23:00:21 -0300

    discrimina los caracteres que no corresponde a digitos

--- tarea1/main.cpp
+++ tarea1/main.cpp
@@ -1,4 +1,4 @@
-/* 
+/*
  * File:   main.cpp
  * Author: claudio
  *
@@ -12,10 +12,10 @@
 using namespace std;
 
 /*
- * 
+ *
  */
 void Separador(){
-    
+
 }
 
 int main(int argc, char** argv) {
@@ -25,16 +25,16 @@ int main(int argc, char** argv) {
         cout<< "Argumento["<<i<<"]:"<<argv[i]<<"\n";
         }
    int largocadena=strlen(argv[2]);
-   
+
    cout<<largocadena<<"\n";
    cout<<"----------------------"<<endl;
    for (int i = 0; i < largocadena; i++) {
        cout<<argv[2][i]<<"-";
    }
    cout<<"-----------------------"<<endl;
-   
+
    std::string polinomio =argv[2];
-   
+
    int vector [largocadena];
    for (int i = 0; i < largocadena; i++) {
         vector[i]=0;
@@ -42,37 +42,42 @@ int main(int argc, char** argv) {
    char Letra;
 
    for (int i = 0; i < largocadena; i++) {//Funcion de Transformacion de String a un Vector
-        
-        Letra = polinomio[i];
-       
-       int aux = Letra -48;
-       vector[i]=aux;
+    if(isdigit(polinomio[i])!=0)// comprueba si el elemento es un digito,donde 0 es la condicion falsa.
+    {
+
+            Letra = polinomio[i];
+
+            int aux = Letra -48;
+
+        vector[i]=aux;
+       }
+
+
        cout<<vector[i]<<"-";
        }
- 
    cout<<endl;
-   
+
    int b=1;//Menu para Ejecurtar las Diferentes Funciones de la Tarea
    int c=1;
    int d=1;
    b=  strcmp(argv[1],"-i");
    c=  strcmp(argv[1],"-v");
    d= strcmp(argv[1],"-g");
-   if(b==0) 
+   if(b==0)
        cout<<"Ceros del Polinomio";
    if(d==0)
        cout<<"Integral";
   if(c==0){
       cout<<"Claudio Acuña";
   }
-   
+
 //   cout<<polinomio;
 //   cout<<"\n"<<cout<<"----------------------"<<endl;
 //   int polinomioentero;
 //   polinomioentero = atoi(polinomio.c_str());
 //   cout <<polinomioentero<<endl;
 //int gradopolinomio= (char)argv[2][2];
-//cout <<"grado = "<<gradopolinomio;   
+//cout <<"grado = "<<gradopolinomio;
     return 0;
 }
 

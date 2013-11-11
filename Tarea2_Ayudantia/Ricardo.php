61833a3e14f651f76014956311cee3c3976bf7bd
claudio <claudioacunacuna@gmail.com>
2013-11-10 23:35:50 -0300

    Se Agrega pagina inicial tarea2 con css

---
+++ Tarea2_Ayudantia/Ricardo.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>This is test</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" type="text/css" href="stylesheet2.css">
+        <script src="http://code.jquery.com/jquery-2.0.3.min.js"></script>
+        <script src='scriptPrincipal2.js'></script>
+    </head>
+    <body>
+        <div id='contenido'>
+            <header>
+                <hgroup>
+                    <h1>Tarea Nº2</h1>
+                </hgroup>
+             <nav>
+                <ul>
+                    <li><a href='index.php'>Claudio Acuña</a></li>
+                    <li><a href='Ricardo.php'>Ricardo Soto</a></li>
+                    <li><a href='Jose.php'>Jose Acuña</a></li>
+                </ul>
+            </nav>
+            </header>
+            <section>
+                <div id='textopr'>
+                    <article>
+                        <hgroup>
+                            <h1>Ricardo Soto</h1>
+                        </hgroup>
+                            <p>Nombre: Ricardo Soto</p>
+                            <p> Rut:nnnnnn-n</p>
+                           <p> Carrera:Ingenieria Civil en Computación e Informatica.</p>
+                          <p>  Asignatura: Ingenieria en Software</p>
+                    </article>
+                    <aside>
+                        <p>
+                            Visita Nuestro Repositorio <a href="https://github.com/claudioac/Ing_Software_2013_2"> Github</a>
+                        </p>
+
+                    </aside>
+                </div>
+            </section>
+
+        </div>
+
+    </body>
+</html>
---
+++ Tarea2_Ayudantia/index.php
@@ -0,0 +1,52 @@
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>This is test</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" type="text/css" href="stylesheet2.css">
+        <script src="http://code.jquery.com/jquery-2.0.3.min.js"></script>
+        <script src='scriptPrincipal2.js'></script>
+    </head>
+    <body>
+        <div id='contenido'>
+            <header>
+                <hgroup>
+                    <h1>Tarea Nº2</h1>
+                </hgroup>
+             <nav>
+                <ul>
+                    <li><a href='index.php'>Claudio Acuña</a></li>
+                    <li><a href='Ricardo.php'>Ricardo Soto</a></li>
+                    <li><a href='Jose.php'>Jose Acuña</a></li>
+                </ul>
+            </nav>
+            </header>
+            <section>
+                <div id='textopr'>
+                    <article>
+                        <hgroup>
+                            <h1>Claudio Acuña</h1>
+                        </hgroup>
+<!--                        <img src="claudio.jpg" alt="" >-->
+                            <p>Nombre: Claudio Acuña</p>
+                            <p>Rut: 17.674.349-2</p>
+                           <p> Carrera: Ingenieria en Informatica.</p>
+                          <p>  Asignatura: Ingenieria en Software</p>
+                    </article>
+                    <aside>
+                        <p>
+                            Visita Nuestro Repositorio <a href="https://github.com/claudioac/Ing_Software_2013_2"> Github</a>
+                        </p>
+
+                    </aside>
+                </div>
+            </section>
+
+        </div>
+
+    </body>
+</html>
